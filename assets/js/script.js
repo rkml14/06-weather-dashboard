af1eb8b59b551771ad92b7e96c29af0f1afcd35f
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-01-28 18:07:47 -0500

    did some changes to index.html, script is not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
 //Global variables
 
+var APIKey = "304b8acf30a32bcf0df086074838b6ea";
+var cityName;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;;
 
-var cityName = '';
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=04b8acf30a32bcf0df086074838b6ea";
+
 
 
 fetch(queryURL);
+
+
+
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+function currentWeather(queryURL) {
+
+    
+}
