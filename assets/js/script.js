6174c076c2a01f8f905590e6fec526bb7e7aa273
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-01-29 12:26:41 -0500

    updated index, & plan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,31 @@
 //Global variables
+let cityFormEl = $('#city-form');
+let cityInputEl = $('#city-input');
 
-const cityInput = document.querySelector('city-form');
-const cityName = document.querySelector('city-input')
 const city = document.querySelector('city');
 const date = document.querySelector('date');
 const temp = document.querySelector('temp');
 const humidity = document.querySelector('humidity');
 const wind = document.querySelector('wind');
 const testCity = "Toronto";
+const testCity2 = "Paris";
 
 let APIKey = "304b8acf30a32bcf0df086074838b6ea";
-let fiveDay = "http://api.openweathermap.org/data/2.5/weather?q=" + testCity + "&units=metric&&appid=" + APIKey;
+let fiveDay = "http://api.openweathermap.org/data/2.5/weather?q=" + testCity2 + "&limit=1&&units=metric&&appid=" + APIKey;
 let currentDay = ""
 let lat
 let lon
 
-let formSubmitHandler = function (event) {
-    event.preventDefault();
+// let formSubmitHandler = function (event) {
+//     event.preventDefault();
   
-    let username = cityInput.value.trim();
-  };
+//     let cityName = cityFormEl.value.trim();
+//   };
 
 
  //trying to fetch 5 day- but only works with test city.  trying to do none test city 
+
+ 
 fetch(fiveDay)
 .then(function (response) {
     return response.json();
@@ -44,10 +47,6 @@ function currentWeather() {
 currentWeather();
 
 
-
-// http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}
-
-
 // // function to find the current weather using latitude and longitude
 function currentWeather() {
     let apiUrl = 'http://api.openweathermap.org/geo/1.0/reverse?lat=51.5098&lon=-0.1180&limit=5&appid=304b8acf30a32bcf0df086074838b6ea'
@@ -63,10 +62,29 @@ function currentWeather() {
 
 
 
-var formSubmitCity = function (event) {
-  event.preventDefault();
+    // Being called when city is submitted 
+$("#city-form").on("submit", function(event) {   //id from form html
+    event.preventDefault();
+    
+    // get name of city searched
+    var cityName = $("#city-input").val(); //id from label html
 
-  var username = nameInputEl.value.trim();
+    if (cityName === "" || cityName == null) {  //prevent no data entry by user 
+        //send alert if search input is empty when submitted
+        alert("Please enter a city name");
+        event.preventDefault();
+    }
+    //  else {
+    //     // function would need to go here to run the whole thing 
+    // }
+});
+
+
+
+// var formSubmitCity = function (event) {
+//   event.preventDefault();
+
+//   var username = nameInputEl.value.trim();
 
   // if (username) {
   //   getUserRepos(username);
@@ -76,8 +94,8 @@ var formSubmitCity = function (event) {
   // } else {
   //   alert('Please enter a GitHub username');
   // }
-};
+// };
 
-cityInput.addEventListener('submit', formSubmitCity);  //call the function for the button
+// cityForm.addEventListener('submit', formSubmitCity);  //call the function for the button
 
 
